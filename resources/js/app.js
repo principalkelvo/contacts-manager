fc930dcf13beb73fce3504556ea541342950f388
principal kelvo <kelvinnjoroowino@gmail.com>
2024-07-29 10:50:39 +0300

    inertia error

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,31 +1,17 @@
 import './bootstrap';
 import '../css/app.css';
 
-import { createApp, h } from 'vue';
-import { createInertiaApp } from '@inertiajs/vue3';
-import { resolvePageComponent } from 'laravel-vite-plugin/inertia-helpers';
-import { ZiggyVue } from '../../vendor/tightenco/ziggy';
-
-const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
+import { createApp, h } from "vue";
+import { createInertiaApp } from "@inertiajs/vue3";
 
 createInertiaApp({
-    title: (title) => `${title} - ${appName}`,
-    resolve: (name) => resolvePageComponent(`./Pages/${name}.vue`, import.meta.glob('./Pages/**/*.vue')),
+    resolve: (name) => {
+        const pages = import.meta.glob("./Pages/**/*.vue", { eager: true });
+        return pages[`./Pages/${name}.vue`];
+    },
     setup({ el, App, props, plugin }) {
-        // Remove the data-page attribute before mounting the app
-        const cleanedProps = { ...props };
-        if (cleanedProps.initialPage) {
-            delete cleanedProps.initialPage;
-        }
-        if (cleanedProps.initialComponent) {
-            delete cleanedProps.initialComponent;
-        }
-        if (cleanedProps.resolveComponent) {
-            delete cleanedProps.resolveComponent;
-        }
-        return createApp({ render: () => h(App, props) })
+        createApp({ render: () => h(App, props) })
             .use(plugin)
-            .use(ZiggyVue)
             .mount(el);
     },
 });
